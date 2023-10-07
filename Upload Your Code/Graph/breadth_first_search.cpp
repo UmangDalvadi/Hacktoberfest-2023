2d067c4fee5e20cf79557acfb161757cef9a8e97
Vaibhav P <vaibhav67979@gmail.com>
2023-10-07 15:26:32 +0530

    Create breadth_first_search.cpp

---
+++ Upload Your Code/Graph/breadth_first_search.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Graph {
+private:
+    int V;
+    vector<vector<int>> adj;
+
+public:
+    Graph(int v) : V(v) {
+        adj.resize(V);
+    }
+
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    void BFS(int startVertex) {
+        vector<bool> visited(V, false);
+        queue<int> q;
+
+        visited[startVertex] = true;
+        q.push(startVertex);
+
+        while (!q.empty()) {
+            int currentVertex = q.front();
+            cout << currentVertex << " ";
+            q.pop();
+
+            for (const int& neighbor : adj[currentVertex]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.push(neighbor);
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    Graph g(7);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+    g.addEdge(2, 5);
+    g.addEdge(2, 6);
+
+    cout << "Breadth-First Traversal (starting from vertex 0): ";
+    g.BFS(0);
+
+    return 0;
+}
