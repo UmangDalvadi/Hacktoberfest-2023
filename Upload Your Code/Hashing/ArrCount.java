1b10df5aa9017b0aa1abdbae3453a10e45146dc1
Naman <namanpokhriyal03@gmail.com>
2023-10-09 18:33:39 +0530

    Added to Hashing java

--- Upload Your Code/Hashing/ArrCount.java
+++ Upload Your Code/Hashing/ArrCount.java
@@ -2,18 +2,13 @@ import java.util.HashMap;
 
 public class ArrCount {
     public static void main(String[] args) {
-        int[] myArray = {1, 2, 3, 1, 2, 1, 4, 5, 4, 3};
-        HashMap<Integer, Integer> occurrenceCount = countOccurrences(myArray);
-        System.out.println(occurrenceCount);
-    }
-
-    public static HashMap<Integer, Integer> countOccurrences(int[] arr) {
+        int[] arr = {1, 2, 3, 1, 2, 1, 4, 5, 4, 3};
         HashMap<Integer, Integer> occurrenceCount = new HashMap<>();
 
-        for (int element : arr) {
-            occurrenceCount.put(element, occurrenceCount.getOrDefault(element, 0) + 1);
-        }
+            for (int element : arr) {
+                occurrenceCount.put(element, occurrenceCount.getOrDefault(element, 0) + 1);
+            }
 
-        return occurrenceCount;
+        System.out.println(occurrenceCount);
     }
 }
